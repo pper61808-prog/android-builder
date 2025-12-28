84cc54a906e526f62b1804b71962550f8184244d
pper61808-prog <pper61808@gmail.com>
2025-12-29 04:18:25 +0700

    Add files via upload

---
+++ MyApp/app/gen/com/mycompany/myapp/BuildConfig.java
@@ -0,0 +1,8 @@
+/** Automatically generated file. DO NOT MODIFY */
+
+/** Regard By Gymkhana Studio **/
+package com.mycompany.myapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
