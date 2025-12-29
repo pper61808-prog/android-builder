bd2047a72bbf61ee1f7dca1d9bb09264ca6c3da4
pper61808-prog <pper61808@gmail.com>
2025-12-29 09:42:27 +0700

    Delete MyApp directory

--- MyApp/app/gen/com/mycompany/myapp/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-
-/** Regard By Gymkhana Studio **/
-package com.mycompany.myapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
